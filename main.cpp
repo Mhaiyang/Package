2bf5c2d3fd70f2bd942546223bb0a30999c4c8d6
TaylorMei <845879017@qq.com>
2017-10-25 11:07:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-#include <iostream>  
+#include <iostream>
 #include <fstream>
-#include <string>  
-#include <vector>  
-#include <algorithm>  
-#include <windows.h>  
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+#include <wincrypt.h>
 
 #define  PACK_MAX_VOLUME  75	// 背包最大体积
-#define  PACK_MAX_WEIGHT  80    // 背包最大承重  
-#define  MAX_GENERATION   100   // 遗传的最大代数  
+#define  PACK_MAX_WEIGHT  80    // 背包最大承重
+#define  MAX_GENERATION   50    // 遗传的最大代数
 
-// 获取一个随机整数0-65535  
+// 获取一个随机整数0-65535
 static unsigned int Rand()
 {
 	unsigned int rand;
@@ -22,28 +23,20 @@ static unsigned int Rand()
 	return rand;
 }
 
-// 获取一个0-1之间的随机浮点数  
+// 获取一个0-1之间的随机浮点数
 static float Rand_Float()
 {
 	return (Rand() % 1000 / 1000.0);
 }
 
-static std::string Int_2_String(int n)
-{
-	char buf[12] = { 0 };
-	sprintf_s(buf, 12, "%d", n);
-	return buf;
-}
-
-
-// 物品定义  
+// 物品定义
 struct Item
 {
 	Item(int va, int vo, int we) : value(va), volume(vo), weight(we) {}
 
 	int value;		//价值
-	int volume;     //体积  
-	int weight;     //重量  
+	int volume;     //体积
+	int weight;     //重量
 };
 
 // 遗传 - 个体定义    每个个体就是一种选择方案
@@ -66,9 +59,9 @@ struct Entity
 
 	int         fitness;        // Value
 	int			sum_volume;		// Volume
-	int         sum_weight;     // Weights  
-	int         generation_id;  // The optimal individual records this value  
-	std::vector<int>  gene;     // gene 
+	int         sum_weight;     // Weights
+	int         generation_id;  // The optimal individual records this value
+	std::vector<int>  gene;     // gene
 };
 
 
@@ -76,7 +69,7 @@ struct Entity
 class GA
 {
 public:
-	static bool EntitySort(const Entity &one, const Entity &two) { return (one.fitness <= two.fitness); }
+	static bool EntitySort(const Entity &one, const Entity &two) { return (one.fitness < two.fitness); }
 
 	static float TotalFitness(const std::vector<Entity> &group)
 	{
@@ -116,10 +109,10 @@ public:
 		}
 	}
 
-	// 显示最优个体  
+	// 显示最优个体
 	void PrintOptimal()
 	{
-		std::cout << "The optimal solution is in " + Int_2_String(m_best_entity.generation_id) + " generation." << std::endl;
+		std::cout << "\nThe optimal solution is in " << m_best_entity.generation_id << " generation." << std::endl;
 		std::cout << "Total value:" << m_best_entity.fitness << "  Total volume:" << m_best_entity.sum_volume << "  Total weight:" << m_best_entity.sum_weight << std::endl;
 		std::cout << "Final Solution: ";
 		for (size_t i = 0; i < m_best_entity.gene.size(); i++)
@@ -127,32 +120,38 @@ public:
 			std::cout << m_best_entity.gene[i] << " ";
 		}
 		std::cout << std::endl;
+		//std::cout << "21709155    " << "梅海洋    " << "计算机科学与技术学院" << "\n";
 	}
 
 private:
-	// Initialize the collection of items 
+	// Initialize the collection of items
 	void Init_Goods()
 	{
-		std::cout << "Information of products.\nValue  Volume  Weight：\n";
+		std::cout << "Information of products.\nItem   Value    Volume    Weight：\n";
 		std::ifstream infile("1.csv");
+		if(!infile.good())
+		{
+		    std::cout << "File does not exist!" << "\n";
+		    exit(0);
+		}
 		while (infile.good())
 		{
 			for (int i = 0; i < m_goods_num; i++)
 			{
 				std::string va,vo,we;
-				getline(infile, va, ' ');
-				getline(infile, vo, ' ');
-				getline(infile, we, ' ');
+				getline(infile, va, ',');
+				getline(infile, vo, ',');
+				getline(infile, we, ',');
 				m_goods.push_back( Item( atoi(va.c_str()), atoi(vo.c_str()), atoi(we.c_str()) ) );
 			}
 		}
 		for (int i = 0; i < m_goods_num; i++)
 		{
-			std::cout << "物品" << i << "  价值：" << m_goods[i].value << "  体积：" << m_goods[i].volume << "  重量：" << m_goods[i].weight << "\n";
+			std::cout << "物品" << i+1 << "  价值：" << m_goods[i].value << "  体积：" << m_goods[i].volume << "  重量：" << m_goods[i].weight << "\n";
 		}
 	}
 
-	// Initialize the population  
+	// Initialize the population
 	void Init_Group()
 	{
 		m_best_entity.InitGene(m_goods_num);	// At begin, best gene is all zero.
@@ -164,11 +163,11 @@ private:
 			m_group.push_back(Entity(m_goods_num));
 			Entity &en = m_group.back();
 
-			// 产生一个0.5-1.0倍的承重值  
+			// 产生一个0.5-1.0倍的承重值
 			float vir_capacity_volume = ((Rand() % 50 + 50) / 100.0) * PACK_MAX_VOLUME;
 			float vir_capacity_weight = ((Rand() % 50 + 50) / 100.0) * PACK_MAX_WEIGHT;
 
-			// 初始化该个体基因中的每一位  
+			// 初始化该个体基因中的每一位
 			volume = 0;
 			weight = 0;
 			count = 0;
@@ -176,7 +175,7 @@ private:
 			{
 				int idx = Rand() % m_goods_num;
 
-				// 连续3次都随机到基因中的为1的染色体，则该个体初始化结束  
+				// 连续3次都随机到基因中的为1的染色体，则该个体初始化结束
 				if (count == 3) { break; }
 				if (en.gene[idx]) { count++; continue; }
 
@@ -187,7 +186,7 @@ private:
 		}
 	}
 
-	// 适应度计算  
+	// 适应度计算
 	void CalcFitness()
 	{
 		for (size_t i = 0; i < m_group.size(); i++)
@@ -200,7 +199,7 @@ private:
 				{
 					val += m_goods[j].value;
 					vol += m_goods[j].volume;
-					wei += m_goods[j].weight;	
+					wei += m_goods[j].weight;
 				}
 			}
 
@@ -212,7 +211,7 @@ private:
 		}
 	}
 
-	// 记录最优个体  
+	// 记录最优个体
 	void RecordOptimalEntity(int gid)
 	{
 		std::stable_sort(m_group.begin(), m_group.end(), GA::EntitySort);	//Sort from small to large
@@ -223,8 +222,8 @@ private:
 			m_best_entity.generation_id = gid;
 		}
 
-		std::cout << "Group Number: " + Int_2_String(m_group.size()) + "  Generation: " + Int_2_String(gid) +"th  Value: " + Int_2_String(en.fitness) + "  Volume: "+Int_2_String(en.sum_volume)
-			+ "  Weight: " + Int_2_String(en.sum_weight) + "\nSolution: ";
+		std::cout << "Group Number: " << m_group.size() << "  Generation: " << gid << "th  Value: " << en.fitness << "  Volume: " << en.sum_volume
+			<< "  Weight: " << en.sum_weight << "\nSolution: ";
 		for (size_t i = 0; i < en.gene.size(); i++)
 		{
 			std::cout << en.gene[i] << ",";
@@ -232,7 +231,7 @@ private:
 		std::cout << std::endl;
 	}
 
-	// 选择  
+	// 选择
 	void Select()
 	{
 		std::vector<Entity>  new_group;
@@ -241,7 +240,7 @@ private:
 
 		int src_group_num = m_group.size();
 
-		// 保留群体中最优的前10%  
+		// 保留群体中最优的前10%
 		int reserve_num = (int)(src_group_num * 0.1);
 		for (int i = 0; i < reserve_num; i++)
 		{
@@ -249,7 +248,7 @@ private:
 			m_group.pop_back();
 		}
 
-		// 计算剩余个体的累积概率  
+		// 计算剩余个体的累积概率
 		std::vector<float>  selected_rate;
 		float sum_fitness = GA::TotalFitness(m_group);
 		selected_rate.push_back(m_group[0].fitness / sum_fitness);
@@ -259,7 +258,7 @@ private:
 			selected_rate.push_back(cur_rate);
 		}
 
-		// 利用轮赌法选择剩下的40%  
+		// 利用轮赌法选择剩下的40%
 		int left_num = (int)(src_group_num * 0.4);
 		for (int i = 0; i < left_num; i++)
 		{
@@ -274,15 +273,15 @@ private:
 			}
 		}
 
-		// 新群体赋值  
+		// 新群体赋值
 		m_group.clear();
 		m_group = new_group;
 	}
 
-	// 是否交叉  
+	// 是否交叉
 	bool IsCross() { return (Rand_Float() <= m_cross_rate); }
 
-	// 交叉  
+	// 交叉
 	void Cross()
 	{
 		size_t src_group_num = m_group.size();
@@ -307,10 +306,10 @@ private:
 		}
 	}
 
-	// 是否变异  
+	// 是否变异
 	bool IsVariation() { return (Rand_Float() <= m_varia_rate); }
 
-	// 变异  
+	// 变异
 	void Variation()
 	{
 		for (size_t i = 0; i < m_group.size(); i++)
@@ -330,14 +329,14 @@ private:
 	}
 
 private:
-	int					m_group_num;    // 群体数量  
-	float               m_cross_rate;   // 交叉概率  
-	float               m_varia_rate;   // 变异概率  
-	int					m_goods_num;    // 物品总数  
-	Entity              m_best_entity;  // 最优个体  
+	int					m_group_num;    // 群体数量
+	float               m_cross_rate;   // 交叉概率
+	float               m_varia_rate;   // 变异概率
+	int					m_goods_num;    // 物品总数
+	Entity              m_best_entity;  // 最优个体
 
-	std::vector<Entity>       m_group;    // 遗传 - 群体  
-	std::vector<Item>		  m_goods;    // 总的物品集合  
+	std::vector<Entity>       m_group;    // 遗传 - 群体
+	std::vector<Item>		  m_goods;    // 总的物品集合
 };
 
 
